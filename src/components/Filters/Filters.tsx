bfbafb63eb0d2e4f8d89990364bbe1afcd260762
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-05-19 23:58:58 +0300

    feat: added finder component

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Button, Container, Input, NumberInput, Select, Slider } from '@mantine/core';
-import './filters.css'
+import { Button, Container, NumberInput, Select } from '@mantine/core';
+import './filters.css';
 
 function Filters() {
   const [selectedOption, setSelectedOption] = useState<string>('');
@@ -8,9 +8,6 @@ function Filters() {
   const handleOptionChange = (value: string) => {
     setSelectedOption(value);
   };
-
-
-
 
   const [inputValue, setInputValue] = useState<number | ''>('');
 
@@ -20,40 +17,37 @@ function Filters() {
     }
   };
   return (
-    <Container className='filters_component' size="315px" style={{ marginBottom: '20px', border: 'solid 2px #EAEBED', borderRadius: '12px', padding: '20px', margin: '0'}}>
-      <div className='header__container'>
-        <h2 className='component__title'>Фильтры</h2>
-            <Button className='button'>Сбросить все x</Button>
+    <Container className="filters_component" size="315px" style={{ marginBottom: '20px', border: 'solid 2px #EAEBED', borderRadius: '12px', padding: '20px', margin: '0' }}>
+      <div className="header__container">
+        <h2 className="component__title">Фильтры</h2>
+            <Button className="button">Сбросить все x</Button>
       </div>
 
-      <div className='filters'>
-      <h2 className='filters_title'>Отрасль</h2>
+      <div className="filters">
+      <h2 className="filters_title">Отрасль</h2>
       <Select
-        className='filter__select'
+        className="filter__select"
         data={['Option 1', 'Option 2', 'Option 3']}
         value={selectedOption}
         onChange={handleOptionChange}
         placeholder="Выберите отрасль"
       />
-    <h2 className='filters_title'>Оклад</h2>
-    <NumberInput 
-    className='price_from'
-     placeholder="От"
-     value={inputValue}
-     onChange={handleChange}
-     >
-    </NumberInput>
-    <NumberInput 
-    className='price_to'
-     placeholder="До"
-     value={inputValue}
-     onChange={handleChange}
-     >
-    </NumberInput>
-    <Button className='apply'>Применить</Button>
-
+    <h2 className="filters_title">Оклад</h2>
+    <NumberInput
+      className="price_from"
+      placeholder="От"
+      value={inputValue}
+      onChange={handleChange}
+    />
+    <NumberInput
+      className="price_to"
+      placeholder="До"
+      value={inputValue}
+      onChange={handleChange}
+    />
+      <Button className="apply">Применить</Button>
       </div>
-      
+
     </Container>
   );
 }
--- src/components/Find/Find.tsx
+++ src/components/Find/Find.tsx
@@ -1,9 +1,27 @@
-import { Input } from "@mantine/core";
+import { Button, Container, Input } from '@mantine/core';
+import './find.css';
 
 function Find() {
-    return <div>
-        <Input></Input>
-    </div>
+    return (
+        <Container className="find_component">
+          <div className="search-container">
+            <svg className="image" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.468 10.468L13.5714 13.5714M12.0924 6.54622C12.0924 9.60931 9.60931 12.0924 6.54622 12.0924C3.48313 12.0924 1 9.60931 1 6.54622C1 3.48313 3.48313 1 6.54622 1C9.60931 1 12.0924 3.48313 12.0924 6.54622Z" stroke="#ACADB9" strokeWidth="1.5" strokeLinecap="round" />
+            </svg>
+            <Input
+              className="finder"
+              placeholder="Введите название вакансии"
+              styles={{
+          input: {
+            border: 'none',
+            padding: '0px',
+          },
+        }}
+            />
+          </div>
+          <Button className="find_button">Поиск</Button>
+        </Container>
+);
 }
 
 export default Find;
