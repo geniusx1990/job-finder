d063fc63236d27047f960a9ac716cd88b77cfb09
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-05-23 04:03:58 +0300

    feat: added pagination

--- src/components/Vaccancies/Vaccancies.tsx
+++ src/components/Vaccancies/Vaccancies.tsx
@@ -1,48 +1,78 @@
 import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import Vacancy from '../Vacancy/Vacancy';
 import { APIData } from '../../api/api';
-import { IVacancy, VacansiesResponse } from '../../interfaces';
+import { IVacancy } from '../../interfaces';
+import './vacancies.css';
 
-interface FiltersProps {
-    accessToken: string | null;
-  }
+interface VacanciesProps {
+  accessToken: string | null;
+}
 
-function Vaccancies({ accessToken }: FiltersProps) {
-  const [vacancies, setVacancies] = useState<VacansiesResponse>([]);
+const pageCount: number = 125;
+const count: number = 4;
 
-if (accessToken) {
-    useEffect(() => {
-        const VaccanciesData = () => {
-          fetch(
-            'https://startup-summer-2023-proxy.onrender.com/2.0/vacancies',
-            {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
-                'X-Api-App-Id': `${APIData.client_secret}`,
-              },
-            }
-            )
-            .then((response) => response.json())
+function Vacancies({ accessToken }: VacanciesProps) {
+  const [vacancies, setVacancies] = useState<IVacancy[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    const VaccanciesData = () => {
+        fetch(
+          `https://startup-summer-2023-proxy.onrender.com/2.0/vacancies?published=1&count=${count}&page=${currentPage}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
+              'X-Api-App-Id': `${APIData.client_secret}`,
+            },
+          }
+        )
+        .then((response) => response.json())
             .then((data) => {
-              console.log(data);
               setVacancies(data.objects);
-})
-            .catch((error) => {
-              console.error('Error fetching access token:', error);
-            });
-        };
+            })
+      .catch((error) => {
+        console.error('Error fetching vacancies:', error);
+      });
+    };
 
-        VaccanciesData();
-      }, []);
-}
-    return (
-        <div>
-            {vacancies.map((vacancy: IVacancy) => (
-            <Vacancy key={vacancy.id} vacancy={vacancy} />
-          ))}
-        </div>
-    );
+    VaccanciesData();
+  }, [accessToken, currentPage]);
+
+  const handlePageClick = (selected: { selected: number }) => {
+    setCurrentPage(selected.selected + 1);
+  };
+
+  return (
+    <div>
+      {vacancies.map((vacancy: IVacancy) => (
+        <Vacancy key={vacancy.id} vacancy={vacancy} />
+      ))}
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={0}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel=""
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </div>
+  );
 }
 
-export default Vaccancies;
+export default Vacancies;
