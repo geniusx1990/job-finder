1635f0419a8669ced366dfdfadbd68bae2944cac
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-05-24 23:39:20 +0300

    feat: create localStorage for save favorites vaccancies

--- src/components/Vacancy/Vacancy.tsx
+++ src/components/Vacancy/Vacancy.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@mantine/core';
 import './vacancy.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { IVacancy } from '../../interfaces';
 
 interface VacancyProps {
@@ -9,9 +10,41 @@ interface VacancyProps {
   }
 
 function Vacancy({ vacancy, accessToken }: VacancyProps) {
-  const { id } = vacancy;
+  const { id, profession, payment_from, payment_to, firm_name, town } = vacancy;
+  const [svgColor, setSvgColor] = useState('none');
+
+  const SVGClick = (event: React.MouseEvent<HTMLOrSVGElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const savedVacancies: IVacancy[] = JSON.parse(localStorage.getItem('vacancies') || '[]');
+
+    if (svgColor === '#5E96FC') {
+      setSvgColor('none');
+      const updatedVacancies = savedVacancies.filter((savedVacancy) => savedVacancy.id !== id);
+      localStorage.setItem('vacancies', JSON.stringify(updatedVacancies));
+
+      console.log(vacancy.town.title);
+    } else {
+      setSvgColor('#5E96FC');
+      const newVacancy: IVacancy = {
+        id,
+        profession,
+        payment_from,
+        payment_to,
+        firm_name,
+        town,
+        type_of_work: vacancy.type_of_work,
+        currency: vacancy.currency,
+        vacancyRichText: vacancy.vacancyRichText,
+      };
+
+      savedVacancies.push(newVacancy);
+      localStorage.setItem('vacancies', JSON.stringify(savedVacancies));
+    }
+  };
     return (
       <Link to={`/vacancy/${id}?accessToken=${accessToken}`} className="vacancy__link">
+
         <Container className="vacancy__containner">
          <div className="vacancy__content">
             <h2 className="vacancy__title">{vacancy.profession}</h2>
@@ -28,11 +61,12 @@ function Vacancy({ vacancy, accessToken }: VacancyProps) {
             <p className="city_title">{vacancy.town.title}</p>
             </div>
          </div>
-         <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <svg width="22" height="20" viewBox="0 0 22 20" fill={svgColor} xmlns="http://www.w3.org/2000/svg" onClick={SVGClick}>
 <path d="M9.97183 1.70846C10.4382 0.933481 11.5618 0.933482 12.0282 1.70847L14.3586 5.58087C14.5262 5.85928 14.7995 6.05784 15.116 6.13116L19.5191 7.15091C20.4002 7.35499 20.7474 8.42356 20.1545 9.10661L17.1918 12.5196C16.9788 12.765 16.8744 13.0863 16.9025 13.41L17.2932 17.9127C17.3714 18.8138 16.4625 19.4742 15.6296 19.1214L11.4681 17.3583C11.1689 17.2316 10.8311 17.2316 10.5319 17.3583L6.37038 19.1214C5.53754 19.4742 4.62856 18.8138 4.70677 17.9127L5.09754 13.41C5.12563 13.0863 5.02124 12.765 4.80823 12.5196L1.8455 9.1066C1.25257 8.42356 1.59977 7.35499 2.48095 7.15091L6.88397 6.13116C7.20053 6.05784 7.47383 5.85928 7.64138 5.58087L9.97183 1.70846Z" stroke="#ACADB9" strokeWidth="1.5" />
          </svg>
         </Container>
       </Link>
+
     );
 }
 export default Vacancy;
